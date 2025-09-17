82482a852a5280ceda9bdb1e9eeeb86257223b7c
AKSHAYA1011 <akshayasudha113@gmail.com>
2025-09-17 21:38:55 +0530

    Added vowels.js

---
+++ vowels.js
@@ -0,0 +1,11 @@
+let str = "Count the vowels in this string";
+let count = 0;
+
+for (let i = 0; i < str.length; i++) {
+    let ch = str[i].toLowerCase();
+    if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+        count++;
+    }
+}
+
+console.log("Number of vowels:", count);
